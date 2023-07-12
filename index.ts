cd6260fb1c1de514e6bb2ffc6f216616d5279b3d
pp449 <mma7710@naver.com>
2023-07-12 15:08:29 +0900

    레디스를 이용한 서버 구성

--- index.ts
+++ index.ts
@@ -1,10 +1,31 @@
 import express, { Express, Request, Response } from "express";
+import { createClient } from "redis";
 
 const app: Express = express();
 const port = 9000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Typescript + Nodejs + Express Server");
+const client = createClient({
+  url: "redis://redis-server:6379",
+});
+
+client.on("error", (error) => {
+  console.error(error);
+});
+
+client.connect();
+
+client.set("number", 0);
+
+app.get("/", async (req: Request, res: Response) => {
+  const number = await client.get("number");
+  if (number) client.set("number", parseInt(number) + 1);
+  res.send("숫자가 1씩 올라갑니다" + number);
+});
+
+let increaseNumber = 0;
+
+app.get("/addUser", (req: Request, res: Response) => {
+  client.set(`user${increaseNumber}`, increaseNumber);
 });
 
 app.listen(port, () => {
